44b86a9f3bd15a5ef96cd75ee09fd3d87d60b9c0
thehandoko <bambang.handoko@outlook.com>
2016-07-04 14:49:40 +0700

    Please do improvement with your code here

---
+++ catalog/view/theme/your_theme/js/common.js
@@ -0,0 +1,86 @@
+/* Embed this code to /catalog/view/theme/your_theme/js/common.js */
+function addToCompare(product_id) { 
+	$.ajax({
+		url: 'index.php?route=product/compare/add',
+		type: 'post',
+		data: 'product_id=' + product_id,
+		dataType: 'json',
+		success: function(json) {
+			$('.success, .warning, .attention, .information').remove();
+						
+			if (json['success']) {
+				$('#notification').html('<div class="alert alert-success alert-dismissible none" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span><span class="sr-only">Success:</span>' + json['success'] + '</div>');
+				
+				$('.alert').fadeIn('slow');
+				
+				$('#compare-total').html(json['total']);
+				
+				$('html, body').animate({ scrollTop: 0 }, 'slow'); 
+			}	
+		}
+	});
+}
+
+function addToCompareManufacturer(product_id) { 
+	$.ajax({
+		url: 'index.php?route=product/manufacturer/add',
+		type: 'post',
+		data: 'product_id=' + product_id,
+		dataType: 'json',
+		success: function(json) {
+			$('.success, .warning, .attention, .information').remove();
+						
+			if (json['success']) {
+				$('#notification').html('<div class="alert alert-success alert-dismissible none" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span><span class="sr-only">Success:</span>' + json['success'] + '</div>');
+				
+				$('.alert').fadeIn('slow');
+				
+				$('#compare-total').html(json['total']);
+				
+				$('html, body').animate({ scrollTop: 0 }, 'slow'); 
+			}	
+		}
+	});
+}
+function addToCompareCategory(product_id) { 
+	$.ajax({
+		url: 'index.php?route=product/category/add',
+		type: 'post',
+		data: 'product_id=' + product_id,
+		dataType: 'json',
+		success: function(json) {
+			$('.success, .warning, .attention, .information').remove();
+						
+			if (json['success']) {
+				$('#notification').html('<div class="alert alert-success alert-dismissible none" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span><span class="sr-only">Success:</span>' + json['success'] + '</div>');
+				
+				$('.alert').fadeIn('slow');
+				
+				$('#compare-total').html(json['total']);
+				
+				$('html, body').animate({ scrollTop: 0 }, 'slow'); 
+			}	
+		}
+	});
+}
+function addToCompareSearch(product_id) { 
+	$.ajax({
+		url: 'index.php?route=product/search/add',
+		type: 'post',
+		data: 'product_id=' + product_id,
+		dataType: 'json',
+		success: function(json) {
+			$('.success, .warning, .attention, .information').remove();
+						
+			if (json['success']) {
+				$('#notification').html('<div class="alert alert-success alert-dismissible none" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span><span class="sr-only">Success:</span>' + json['success'] + '</div>');
+				
+				$('.alert').fadeIn('slow');
+				
+				$('#compare-total').html(json['total']);
+				
+				$('html, body').animate({ scrollTop: 0 }, 'slow'); 
+			}	
+		}
+	});
+}
